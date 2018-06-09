5b843c4940cf60c933af894a7fd5fec47031a600
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-06-09 00:39:13 -0500

    Initial configuration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <h1>Mi primera Aplicación</h1>
+     <p>Esta es mi primera aplicación en React, está padrísimo!"</p>
+    );
+  }
+}
+
+export default App;
